5e3534173479e8d20f70e533952de42cec17f0df
indhumathi43 <49307290+indhumathi43@users.noreply.github.com>
2019-04-20 13:42:48 +0530

    large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+if(a>b and a>c):
+print("A is large")
+elif(b>a and b>c):
+print("B is large")
+else:
+print("C is large")

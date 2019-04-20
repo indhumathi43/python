51a4b5b82b53147680fa6aa254006e383cb57b08
indhumathi43 <49307290+indhumathi43@users.noreply.github.com>
2019-04-20 12:07:54 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,9 +1,9 @@
 a=input("Enter letter:")
 a=a.lower()
 if(a.islower() and len(a)==1):
-if(a in vowel):
-print("VOWEL")
+  if(a in vowel):
+    print("VOWEL")
+  else:
+    print("CONSONANT")
 else:
-print("CONSONANT")
-else:
-print("INVALID")
+  print("INVALID")

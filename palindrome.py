19f1050045711566f6e18dc2950db2a0eeabd1d7
indhumathi43 <49307290+indhumathi43@users.noreply.github.com>
2019-04-21 23:39:07 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+num=int(input("Enter number:"))
+temp=num
+rev=0
+while temp!=0:
+   rev=(rev*10)+(temp%10)
+   temp=temp//10
+if num==rev:
+    print("Palindrome")
+else:
+    print("Not palindrome")
